b918a0761772a6a2fec6812fa2eca9543fb92b3a
WeiChien Hsu <weichien711@gmail.com>
2017-09-22 17:45:14 -0500

    click

---
+++ Click/click.js
@@ -0,0 +1,10 @@
+function handleClicks() {
+  var clickCount = 0;
+  $('.js-click-counter').text(clickCount);
+  $('.js-clicker').click(function(event) {
+    clickCount += 1;
+    $('.js-click-counter').text(clickCount);
+  });
+}
+
+handleClicks();
