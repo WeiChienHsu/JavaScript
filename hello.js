9cb4f28bc58928890c287e5454ec6a561d310d23
WeiChien Hsu <weichien711@gmail.com>
2017-09-24 00:47:55 -0500

    0924

--- hello.js
+++ hello.js
@@ -36,4 +36,10 @@ function countAverage(num1,num2){
 	return (num1+num2)/2;
 }
 
-console.log(conutAverage(4,6));
+
+function sayBye(name){
+	var goodBye = 'Bye!';
+	return goodBye+name;
+}
+
+console.log(sayBye('Kay'));
