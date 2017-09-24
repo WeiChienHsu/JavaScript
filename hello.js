31840befaf0fc5ee9fa99de18bb0a606c04f4fbf
WeiChien Hsu <weichien711@gmail.com>
2017-09-23 22:51:30 -0500

    node

--- hello.js
+++ hello.js
@@ -7,3 +7,33 @@ var person = {
 
 console.log(person.name);
 person.greet();
+
+var stringVar = 'Kevin';
+var numVar = 23;
+var combined = stringVar+numVar;
+
+typeof combined;
+console.log(combined);
+
+function alert(string){
+  console.log(string);
+}
+
+function alertHelloWorld(){
+	alert('Hello World');
+}
+
+alertHelloWorld();
+
+function sayHelloTo(name){
+	var greet = 'Hello!';
+	return greet + name + ".";
+}
+
+console.log(sayHelloTo('Kevin'));
+
+function countAverage(num1,num2){
+	return (num1+num2)/2;
+}
+
+console.log(conutAverage(4,6));
