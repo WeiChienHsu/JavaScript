f03c7be034ffb455c7dc75ee12218221e6e43998
WeiChien Hsu <weichien711@gmail.com>
2017-09-26 22:45:34 -0500

    finished object drill

--- Object/objectplus.js
+++ Object/objectplus.js
@@ -1,210 +1,210 @@
-// var pageViewCounts = {
-// 	homePage:399,
-// 	aboutMePage:450,
-// 	termsOfService:25
-// };
+var pageViewCounts = {
+	homePage:399,
+	aboutMePage:450,
+	termsOfService:25
+};
 
-// Object.keys(pageViewCounts).map(function(keys){
-// 	console.log(
-// 		'the '+ keys +' page has ' + pageViewCounts[keys] + ' views.')
-// })
+Object.keys(pageViewCounts).map(function(keys){
+	console.log(
+		'the '+ keys +' page has ' + pageViewCounts[keys] + ' views.')
+})
 
-// function classMates(name,yearsOld){
-// 	return{
-// 		name:name,
-// 		yearsOld:yearsOld,
-// 		passbirthday:function(){
-// 			console.log(
-// 				"After one Year.....");
-// 			this.yearsOld ++;
-// 		},
+function classMates(name,yearsOld){
+	return{
+		name:name,
+		yearsOld:yearsOld,
+		passbirthday:function(){
+			console.log(
+				"After one Year.....");
+			this.yearsOld ++;
+		},
 
-// 		introductionYourSelf:function(){
-// 			console.log(
-// 				"I'm "+this.name+" "+this.yearsOld+' years old.');
-// 		}
-// 	}
-// }
+		introductionYourSelf:function(){
+			console.log(
+				"I'm "+this.name+" "+this.yearsOld+' years old.');
+		}
+	}
+}
 
-// var kevin = classMates('Kevin',8);
-// kevin.introductionYourSelf();
-// kevin.passbirthday();
-// kevin.introductionYourSelf();
+var kevin = classMates('Kevin',8);
+kevin.introductionYourSelf();
+kevin.passbirthday();
+kevin.introductionYourSelf();
 
-// function makeStudentsReport(data) {
-//   var results = [];
-//   for (var i=0;i<data.length;i++){
-//   	  var item = data[i];
-//   	  results.push(item.name + ': ' + item.grade);
-//   }
-//   return results;
-// }
-
-
-
-// function testIt() {
-
-//   var testData = [
-//     {name: 'Jane Doe', grade: 'A'},
-//     {name: 'John Dough', grade: 'B'},
-//     {name: 'Jill Do', grade: 'A'}
-//   ];
-
-//   var expectations = [
-//     'Jane Doe: A',
-//     'John Dough: B',
-//     'Jill Do: A'
-//   ];
-
-//   var results = makeStudentsReport(testData);
-
-//   if (!(results && results instanceof Array)) {
-//     console.error(
-//       'FAILURE: `makeStudentsReport` must return an array');
-//     return
-//   }
-//   if (results.length !== testData.length) {
-//     console.error(
-//       'FAILURE: test data had length of ' + testData.length +
-//       ' but `makeStudentsReport` returned array of length ' + results.length);
-//     return
-//   }
-//   for (var i=0; i < expectations.length; i++) {
-//     var expect = expectations[i];
-//     if (!results.find(function(item) {
-//       return item === expect;
-//     })) {
-//       console.error(
-//         'FAILURE: `makeStudentsReport` is not ' +
-//         'producing expected strings'
-//       );
-//       return
-//     }
-//   }
-//   console.log('SUCCESS: `makeStudentsReport` is working');
-// }
-
-// testIt();
-
-
-// var studentData = [
-//   {
-//     name: 'Tim',
-//     status: 'Current student',
-//     course: 'Biology'
-//   },
-//   {
-//     name: 'Sue',
-//     status: 'Withdrawn',
-//     course: 'Mathematics'
-//   },
-//   {
-//     name: 'Liz',
-//     status: 'On leave',
-//     course: 'Computer science'
-//   }
-// ];
-
-// function enrollInSummerSchool(students) {
-// 	students.forEach(function(obj){
-// 		obj.status = 'In Summer school';
-// 	});
-
-// 	return students;
-// }
+function makeStudentsReport(data) {
+  var results = [];
+  for (var i=0;i<data.length;i++){
+  	  var item = data[i];
+  	  results.push(item.name + ': ' + item.grade);
+  }
+  return results;
+}
 
 
 
-// function testIt() {
-//   var testData = [
-//     {
-//       name: 'Burt',
-//       status: 'Playing hooky',
-//       course: 'Biology'
-//     },
-//     {
-//       name: 'Melanie',
-//       status: 'Sick',
-//       course: 'Mathematics'
-//     },
-//     {
-//       name: 'Leonard',
-//       status: 'AWOL',
-//       course: 'Computer science'
-//     }
-//   ];
+function testIt() {
 
-//   var results = enrollInSummerSchool(testData);
+  var testData = [
+    {name: 'Jane Doe', grade: 'A'},
+    {name: 'John Dough', grade: 'B'},
+    {name: 'Jill Do', grade: 'A'}
+  ];
 
-//   if (!(results && results instanceof Array)) {
-//     console.error('FAILURE: `enrollSummerSchool` must return an array');
-//     return
-//   }
+  var expectations = [
+    'Jane Doe: A',
+    'John Dough: B',
+    'Jill Do: A'
+  ];
 
-//   for (var i=0; i<testData.length; i++) {
-//     var result = results.find(function(_result) {
-//       return (
-//         _result.name === testData[i].name &&
-//         _result.course === testData[i].course &&
-//         _result.status === 'In Summer school');
-//     });
-//     if (!result) {
-//       console.error(
-//         'FAILURE: `enrollSummerSchool` should return ' +
-//         'original key/value pairs for each student, and ' +
-//         'update `status` to "In Summer school"');
-//       return
-//     }
-//   }
-//   console.info('SUCCESS: `enrollSummerSchool` is working');
-// }
+  var results = makeStudentsReport(testData);
 
-// testIt();
+  if (!(results && results instanceof Array)) {
+    console.error(
+      'FAILURE: `makeStudentsReport` must return an array');
+    return
+  }
+  if (results.length !== testData.length) {
+    console.error(
+      'FAILURE: test data had length of ' + testData.length +
+      ' but `makeStudentsReport` returned array of length ' + results.length);
+    return
+  }
+  for (var i=0; i < expectations.length; i++) {
+    var expect = expectations[i];
+    if (!results.find(function(item) {
+      return item === expect;
+    })) {
+      console.error(
+        'FAILURE: `makeStudentsReport` is not ' +
+        'producing expected strings'
+      );
+      return
+    }
+  }
+  console.log('SUCCESS: `makeStudentsReport` is working');
+}
 
-// function findById(items, idNum) {
-//    for(var i=0;i<items.length;i++){
-//    	var item =  items[i];
-//    	if (item.id === idNum){
-//    		return item;
-//    	}
-//    }
-// }
+testIt();
 
 
-// var scratchData = [
-//   {id: 22, foo: 'bar'},
-//   {id: 28, foo: 'bizz'},
-//   {id: 19, foo: 'bazz'}
-// ];
+var studentData = [
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology'
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics'
+  },
+  {
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science'
+  }
+];
 
-// console.log(findById(scratchData,22));
+function enrollInSummerSchool(students) {
+	students.forEach(function(obj){
+		obj.status = 'In Summer school';
+	});
 
-// function testIt() {
-//   var testData = [
-//     {id: 1, foo: 'bar'},
-//     {id: 2, foo: 'bizz'},
-//     {id: 3, bang: 'boo'}
-//   ];
-//   var result = findById(testData, 3);
-//   if (!(result && result !== null && typeof result === 'object')) {
-//     console.error('`findById` must return an object');
-//     return
-//   }
-//   if (result.id !== 3) {
-//     console.error(
-//       'Asked for item with id of `3` but got back one with id of ' +
-//       result.id
-//     );
-//     return
-//   }
-//   if (result.bang !== 'boo') {
-//     console.error('Expected all key/value pairs from target object to be returned');
-//     return
-//   }
-//   console.log('SUCCESS: `findByid` is working');
-// }
+	return students;
+}
 
-// testIt();
+
+
+function testIt() {
+  var testData = [
+    {
+      name: 'Burt',
+      status: 'Playing hooky',
+      course: 'Biology'
+    },
+    {
+      name: 'Melanie',
+      status: 'Sick',
+      course: 'Mathematics'
+    },
+    {
+      name: 'Leonard',
+      status: 'AWOL',
+      course: 'Computer science'
+    }
+  ];
+
+  var results = enrollInSummerSchool(testData);
+
+  if (!(results && results instanceof Array)) {
+    console.error('FAILURE: `enrollSummerSchool` must return an array');
+    return
+  }
+
+  for (var i=0; i<testData.length; i++) {
+    var result = results.find(function(_result) {
+      return (
+        _result.name === testData[i].name &&
+        _result.course === testData[i].course &&
+        _result.status === 'In Summer school');
+    });
+    if (!result) {
+      console.error(
+        'FAILURE: `enrollSummerSchool` should return ' +
+        'original key/value pairs for each student, and ' +
+        'update `status` to "In Summer school"');
+      return
+    }
+  }
+  console.info('SUCCESS: `enrollSummerSchool` is working');
+}
+
+testIt();
+
+function findById(items, idNum) {
+   for(var i=0;i<items.length;i++){
+   	var item =  items[i];
+   	if (item.id === idNum){
+   		return item;
+   	}
+   }
+}
+
+
+var scratchData = [
+  {id: 22, foo: 'bar'},
+  {id: 28, foo: 'bizz'},
+  {id: 19, foo: 'bazz'}
+];
+
+console.log(findById(scratchData,22));
+
+function testIt() {
+  var testData = [
+    {id: 1, foo: 'bar'},
+    {id: 2, foo: 'bizz'},
+    {id: 3, bang: 'boo'}
+  ];
+  var result = findById(testData, 3);
+  if (!(result && result !== null && typeof result === 'object')) {
+    console.error('`findById` must return an object');
+    return
+  }
+  if (result.id !== 3) {
+    console.error(
+      'Asked for item with id of `3` but got back one with id of ' +
+      result.id
+    );
+    return
+  }
+  if (result.bang !== 'boo') {
+    console.error('Expected all key/value pairs from target object to be returned');
+    return
+  }
+  console.log('SUCCESS: `findByid` is working');
+}
+
+testIt();
 
 var objectB = {
   id: 3,
@@ -306,5 +306,27 @@ function testIt() {
   console.log('SUCCESS: `validateKeys` is working');
 }
 
-testIt()
+testIt();
 
+
+function makeToDos(owner, toDos) {
+  return{
+  	'owner':owner,
+  	'toDos':toDos,
+  	generateHtml:function(){
+  		var html = '<ul>';
+  		this.toDos.forEach(function(todo){
+  			html += "<li>"+todo+'</li>';
+  		});
+  		return html + '</ul>';
+  	}
+  }
+}
+
+
+
+var toDos = ['get milk', 'walk dog', 'pay bills', 'eat dinner'];
+var owner = 'Steve';
+var myToDos = makeToDos(owner, toDos);
+
+console.log(myToDos.generateHtml());
