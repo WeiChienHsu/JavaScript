890c9741a7a7f4053ebc9c82cba737100e484626
WeiChien Hsu <weichien711@gmail.com>
2017-09-27 08:34:17 -0500

    string

--- test.js
+++ test.js
@@ -1,38 +1,45 @@
-function add(num1,num2){
-	return num1+num2;
-}
+// function add(num1,num2){
+// 	return num1+num2;
+// }
 
-function testAdd(){
-	var num1 = 5;
-	var num2 = 10;
-	var expected = 15;
-	if (add(num1,num2)===expected){
-		console.log('Success');
-		return true;
-	}
-	else{
-		console.log('Failure');
-		return false;
-	}
-}
+// function testAdd(){
+// 	var num1 = 5;
+// 	var num2 = 10;
+// 	var expected = 15;
+// 	if (add(num1,num2)===expected){
+// 		console.log('Success');
+// 		return true;
+// 	}
+// 	else{
+// 		console.log('Failure');
+// 		return false;
+// 	}
+// }
 
-testAdd();
+// testAdd();
 
-function shouter(whatToShout){
-	return whatToShout.toUpperCase+'!!!';
-}
+// function shouter(whatToShout){
+// 	return whatToShout.toUpperCase+'!!!';
+// }
 
-function testShouter(){
-	var whatToShout = 'hey,men';
-	var expected = 'HEY,MEN!!!';
-	if (shouter(whatToShout) === expected){
-		console.log('Success');
-		return true;
-	}
+// function testShouter(){
+// 	var whatToShout = 'hey,men';
+// 	var expected = 'HEY,MEN!!!';
+// 	if (shouter(whatToShout) === expected){
+// 		console.log('Success');
+// 		return true;
+// 	}
 
-	else{
-		console.log("Failure");
-		return false;
-	}
-}
+// 	else{
+// 		console.log("Failure");
+// 		return false;
+// 	}
+// }
 
+
+
+var myStr; // Change this line
+myStr = "FirstLine\n\\SecondLine\\\nThirdLine";
+
+
+console.log(myStr);
