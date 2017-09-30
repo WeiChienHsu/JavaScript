e30625880e8ba26676e5099ea94437c30de83fa1
WeiChien Hsu <weichien711@gmail.com>
2017-09-30 18:17:28 -0500

    getElementByxxx

--- dom/dom.js
+++ dom/dom.js
@@ -1,5 +1,14 @@
-var tagid = document.getElementById("list");
-tagid.style.color="red";
+var first = document.getElementById("first");
+first.style.border = "2px pink solid";
 
-var li = document.getElementsByTagName("li");
-li[1].style.color="green";
+var second = document.getElementsByClassName("special");
+second[1].classList.add("large");
+// Give a class to an element
+
+var third = document.getElementsByTagName("p");
+third[2].style.color = "purple";
+
+var forth = document.querySelector("#last");
+forth.style.color = "green";
+
+forth.textContent = "ABC";
